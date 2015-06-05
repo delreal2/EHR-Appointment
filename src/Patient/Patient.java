a9dba5e70f2971b920f1db56b26c5b2ec0b78b29
Austin Vern Songer <austin.v.songer@gmail.com>
2015-06-05 11:13:35 -0500

    EHR

--- src/Patient/Patient.java
+++ src/Patient/Patient.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Java-Project
+ * Daniel, Olena, Austin
+ * Robert Morris University
  */
 package Patient;
 
